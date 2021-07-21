410b8de04e4d66f516197c5aa9a27f58439dd74a
Joshua Ogbonna <devjaykes@gmail.com>
2021-07-21 20:02:12 +0100

    First commit in project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="navbar navbar-expand-lg navbar-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="../assets/logo.png" width="250" height="100" />
+        </a>
+        
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
