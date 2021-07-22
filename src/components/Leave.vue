b57599d5fbd26951ab6029632fe7d3787ec0db47
Joshua Ogbonna <devjaykes@gmail.com>
2021-07-22 17:35:55 +0100

    :Leave section done

--- src/components/Leave.vue
+++ src/components/Leave.vue
@@ -1,55 +1,110 @@
 <template>
-<b-overlay :show="show" rounded="sm">
-  <div class="container leave" :aria-hidden="show ? 'true' : null">
-    <h4>Apply for Leave</h4>
-    <form action="">
+  <b-overlay :show="show" rounded="sm">
+    <div class="container leave" :aria-hidden="show ? 'true' : null">
+      <div class="actions mt-5">
+        <h4>Apply for Leave</h4>
+        <button @click="toggleForm">New Leave</button>
+      </div>
+      <form action="" v-if="showForm">
         <div class="form-group mt-2">
-            <label for="name" class="form-label">Staff Name</label>
-            <input type="text" name="name" class="form-control" v-model="leave.name">
+          <label for="name" class="form-label">Staff Name</label>
+          <input
+            type="text"
+            name="name"
+            class="form-control"
+            v-model="leave.name"
+          />
         </div>
         <div class="form-group mt-2">
-            <label for="code" class="form-label">Staff Code</label>
-            <input type="text" name="code" class="form-control" v-model="leave.staffCode">
+          <label for="code" class="form-label">Staff Code</label>
+          <input
+            type="text"
+            name="code"
+            class="form-control"
+            v-model="leave.staffCode"
+          />
         </div>
         <div class="form-group mt-2">
-            <label for="reason" class="form-label">Reason for Leave</label>
-            <input type="text" name="reason" class="form-control" v-model="leave.reason">
+          <label for="reason" class="form-label">Reason for Leave</label>
+          <input
+            type="text"
+            name="reason"
+            class="form-control"
+            v-model="leave.reason"
+          />
         </div>
         <div class="form-group mt-2">
-            <label for="message" class="form-label">Message</label>
-            <textarea name="message" cols="30" rows="5" class="form-control" v-model="leave.message"></textarea>
+          <label for="message" class="form-label">Message</label>
+          <textarea
+            name="message"
+            cols="30"
+            rows="5"
+            class="form-control"
+            v-model="leave.message"
+          ></textarea>
         </div>
         <button class="mt-2" @click.prevent="leaveApply">Sumbit</button>
-    </form>
-  </div>
-</b-overlay>
+      </form>
+
+      <div class="leave__table mt-5" v-if="this.$store.getters.user.leaves.length > 0">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Reason</th>
+              <th scope="col">Leave</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="leave in this.$store.getters.user.leaves"
+              :key="leave._id"
+            >
+              <th scope="row">{{ leave.reason }}</th>
+              <td>{{ leave.message }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        <p class="text-center">You do not have any leave</p>
+      </div>
+    </div>
+  </b-overlay>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            leave: {
-                name: this.$store.getters.user.name,
-                staffCode: this.$store.getters.user.staffCode,
-                reason: '',
-                message: ''
-            },
-            show: false
-        }
+  data() {
+    return {
+      leave: {
+        name: this.$store.getters.user.name,
+        staffCode: this.$store.getters.user.staffCode,
+        reason: "",
+        message: "",
+      },
+      show: false,
+      showForm: false,
+    };
+  },
+  methods: {
+    toggleForm() {
+      this.showForm = true;
     },
-    methods: {
-        leaveApply() {
-            this.show = true
-            this.$store.dispatch('POSTLEAVE', this.leave).then(() => {
-                this.show = false
-                this.$store.dispatch('GETPROFILE')
-            }).catch(err => {
-                console.log(err)
-                this.show= true
-            })
-        }
+    leaveApply() {
+      this.show = true;
+      this.$store
+        .dispatch("POSTLEAVE", this.leave)
+        .then(() => {
+          this.showForm = false;
+          this.show = false;
+          this.$store.dispatch("GETPROFILE");
+        })
+        .catch((err) => {
+          console.log(err);
+          this.show = true;
+        });
     },
+  },
 };
 </script>
 
@@ -61,12 +116,16 @@ export default {
   display: block;
 }
 form {
-    width: 100%;
+  width: 100%;
 }
-form button {
-    background: #185adb;
-    color: #fff;
-    border: none;
-    padding: 9px 18px;
+button {
+  background: #185adb;
+  color: #fff;
+  border: none;
+  padding: 9px 18px;
+}
+.actions {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
